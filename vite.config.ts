ff14bf52a254dece7577ea28e24f1a9f8566051a
shiningsudipto <shiningsudipto.com>
2025-07-25 13:40:05 +0600

    removed unused installed components and added form test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   test: {
     globals: true,
